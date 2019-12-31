57d40b38080233a22d79c6cf0e447f85acfff2c6
HN67 <hn67allardkids@gmail.com>
2019-12-30 19:07:20 -0700

    Change to use nsapi, add logging

--- endorsements.py
+++ endorsements.py
@@ -1,31 +1,58 @@
 """Determines who has not been endorsed on NationStates"""
 
-# Import lxml
-from lxml import etree
+# Import logging
+import logging
+
+# Import nsapi
+import nsapi
+from nsapi import NationStandard as Nation
+
+# Set logging level
+level = logging.INFO
+logging.basicConfig(level=level)
+# Name logger
+logger = logging.getLogger()
+# Change nsapi logging level
+nsapi.logger.setLevel(level=level)
+
+# Setup API
+API = nsapi.NSRequester("HN67 API Reader")
 
 # Set endorser nation to check for
-endorser = "hn67"
+endorser = "kuriko"
 
 # Load downloaded nation file
-nationsXML = etree.parse("nations.xml.gz").getroot()
+nationsXML = API.retrieve_nation_dump()
 
 # Pull all nations in 100000 Islands that are WA members
-waMembers = set(
-    # Save nation tag
-    nation for nation in nationsXML
-    # Check region (child [8]) and WA status (child [5])
-    if nation[8].text == "10000 Islands" and nation[5].text == "WA Member"
-)
+logging.info("Collecting 10000 Islands WA Members")
+# Initalize empty list
+waMembers = []
+# Iterate through xml nodes
+for nationNode in nationsXML:
+    # Convert each node to a NationStandard object
+    nation = Nation(nationNode)
+    # If the nation is in XKI and WA, add to list
+    if nation["REGION"].text == "10000 Islands" and nation["UNSTATUS"].text == "WA Member":
+        waMembers.append(nation)
 
 # Pull nations who are not endorsed
+logging.info("Collecting WA members who have not been endorsed")
 unendorsed = [
-    # Save name ([0]) string, converting to lowercase, underscore format
-    nation[0].text.lower().replace(" ", "_") for nation in waMembers
-    # Check if unendorsed by checking endorsements ([6])
-    if nation[6].text is None or endorser not in nation[6].text
+    # Save name string, converting to lowercase, underscore format
+    nation["NAME"].text.lower().replace(" ", "_") for nation in waMembers
+    # Check if unendorsed by checking endorsements
+    if nation["ENDORSEMENTS"].text is None or endorser not in nation["ENDORSEMENTS"].text
 ]
 
 # Output unendorsed nations
+logging.info("Outputting results\n")
+# Header
+print(f"{endorser} has not endorsed the following WA Members in 10000 Islands:")
+# Print each nation, with a URL generated
 for step, nation in enumerate(unendorsed):
     # Increment step so that it is 1-based
     print(f"{step+1}. https://www.nationstates.net/nation={nation}")
+
+# Wait for user input before ending (prevents closing command prompt)
+input("Press <Enter> to exit")
