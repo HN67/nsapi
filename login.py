73cdd651623a17d73a6d063d69b654cb2b930e03
HN67 <hn67allardkids@gmail.com>
2020-07-03 23:22:30 -0600

    Make login script

---
+++ login.py
@@ -0,0 +1,34 @@
+"""Pings a nation to register activity, and also returns an autologin"""
+
+import getpass
+import logging
+
+import config
+import nsapi
+
+# Set logging level
+level = logging.INFO
+logging.basicConfig(level=level)
+# Name logger
+logger = logging.getLogger()
+# Change nsapi logging level
+nsapi.logger.setLevel(level=level)
+
+
+def main() -> None:
+    """Main function"""
+
+    requester = nsapi.NSRequester(config.userAgent)
+
+    nation = input("Nation: ")
+
+    try:
+        autologin = requester.get_autologin(nation, getpass.getpass("Password: "))
+    except KeyError:
+        print("Something went wrong, likely incorrect password")
+    else:
+        print(f"Autologin: {autologin}")
+
+
+if __name__ == "__main__":
+    main()
