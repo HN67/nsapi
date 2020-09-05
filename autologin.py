e20a5df112fb00bb7ce070530b0ee7e1c9f45b25
HN67 <hn67allardkids@gmail.com>
2020-09-04 22:21:57 -0600

    Make it also print out region

--- autologin.py
+++ autologin.py
@@ -1,5 +1,8 @@
 """Autologins a list of nations, with the option of also generating autologin keys."""
 
+# TODO show regions as well
+
+import dataclasses
 import logging
 from typing import Dict, Mapping, Optional
 
@@ -15,27 +18,36 @@ logger = logging.getLogger()
 nsapi.logger.setLevel(level=level)
 
 
+@dataclasses.dataclass()
+class Result:
+    """Class containing resulting data from autologin method."""
+
+    autologin: str
+    region: str
+
+
 def autologin(
     requester: nsapi.NSRequester, nations: Mapping[str, str], isAutologin: bool
-) -> Mapping[str, Optional[str]]:
+) -> Mapping[str, Optional[Result]]:
     """Autologins a list of nations, from the nations dict of {nation: password/autologin}.
     If isAutologin is true, the passwords are interpreted as autologins.
     Returns a dict mapping from nations to autologins.
     If the autologin value of the returned mapping is None, that means the login failed.
     """
-    output: Dict[str, Optional[str]] = {}
+    output: Dict[str, Optional[Result]] = {}
     for nation, password in nations.items():
         # Check how to interpret password
         if isAutologin:
             nationAPI = requester.nation(nation, nsapi.Auth(autologin=password))
         else:
             nationAPI = requester.nation(nation, nsapi.Auth(password=password))
-        # .ping is true if the authentication was succseful
-        if nationAPI.ping():
-            # This shouldnt make a request since the ping was succsesful
-            output[nation] = nationAPI.get_autologin()
-        else:
+        # Try making the shard request
+        try:
+            shards = nationAPI.shards("region", "ping")
+        except nsapi.AuthError:
             output[nation] = None
+        else:
+            output[nation] = Result(nationAPI.get_autologin(), shards["region"])
     return output
 
 
@@ -77,17 +89,17 @@ def main() -> None:
     output = autologin(requester, nations, isAutologin)
 
     # Summarize results
-    for nation, autologinValue in output.items():
-        if autologinValue:
-            print(f"Successfully logged {nation} in.")
+    for nation, result in output.items():
+        if result:
+            print(f"Successfully logged {nation} in. (Region: {result.region})")
         else:
             print(f"Failed to log in {nation}. Likely an incorrect password.")
 
     # Only generate output if desired
     if outputPath != "":
         with open(nsapi.absolute_path(outputPath), "w") as file:
-            for nation, autologinValue in output.items():
-                print(f"{nation},{autologinValue}", file=file)
+            for nation, result in output.items():
+                print(f"{nation},{result.autologin}", file=file)
 
 
 if __name__ == "__main__":
