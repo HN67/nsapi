7403538fa27ced62b39c14ad42ecd2323ca6abe8
HN67 <hn67allardkids@gmail.com>
2021-05-23 14:48:45 -0600

    Create extremely basic package files for autologin

---
+++ autologin/__init__.py
@@ -0,0 +1,6 @@
+"""Autologin package containing login utilities"""
+
+__all__ = ["autologin", "login"]
+
+from . import autologin
+from . import login
---
+++ autologin/__main__.py
@@ -0,0 +1,6 @@
+"""Main file, delegates to autologin"""
+
+from . import autologin
+
+if __name__ == "__main__":
+    autologin.main()
